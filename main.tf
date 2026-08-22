bc9c52cd89c600a58740877e86580267c81ef729
alex <alexvbh@gmail.com>
2026-08-22 13:22:19 -0300

    chore: remove AWS Amplify resources

--- main.tf
+++ main.tf
@@ -21,9 +21,3 @@ variable "environment" {
   default     = "dev"
   description = "Ambiente da aplicação"
 }
-
-variable "github_access_token" {
-  description = "GitHub Personal Access Token para o AWS Amplify Hosting"
-  type        = string
-  sensitive   = true
-}
